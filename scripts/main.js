205bdb1be406d98da8e88bfbfddc019fca0591f4
erik.roger.fuller <erik.roger.fuller@gmail.com>
2022-10-24 20:11:39 -0400

    super basic image scroller for test

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
+
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Artworks:";
+
+
+const myImage = document.querySelector("img");
+const imageSrcs = ["images/bastion_art.jpg", "images/Church_smaller.png",
+                    "images/cunieform_writer.jpg", "images/planks2.jpg"
+                    ]
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    const i = imageSrcs.indexOf(mySrc);
+    if (i < imageSrcs.length-1){
+        myImage.setAttribute("src", imageSrcs[i]);
+    } else {
+        myImage.setAttribute("src", imageSrcs[0])
+    }
+};
